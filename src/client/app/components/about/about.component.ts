0cde7f3e01c1a43a1bafce28f2b961a897101f27
Peter Vium Skaarup <pvskaarup@gmail.com>
2016-06-22 19:18:15 +0200

    setup

---
+++ src/client/app/components/about/about.component.ts
@@ -0,0 +1,11 @@
+import {BaseComponent} from '../../frameworks/core.framework/index';
+
+@BaseComponent({
+  moduleId: module.id,
+  selector: 'sd-about',
+  templateUrl: 'about.component.html',
+  styleUrls: ['about.component.css']
+})
+export class AboutComponent  {
+  
+}
